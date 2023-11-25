7330e57083e31eee410bb6fc02644a76b4d9ff75
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-11-24 19:47:13 -0500

    Add Scrollbar JS

---
+++ scripts/index.script.js
@@ -0,0 +1,5 @@
+/* Scroll Bar Down */
+window.addEventListener("scroll", function(){
+    var navbar = document.getElementById("navbar");
+    navbar.classList.toggle("down_scroll", window.scrollY>0);
+})
